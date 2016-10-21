1659d8048ca9cd21d92bea2ace8bfe5ba7899836
ckashby <clay.ashby@gmail.com>
2016-10-21 15:55:25 -0700

    init working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Welcome LeashedIn';
+  subtitle = 'Material 2 does 2!'
 }
